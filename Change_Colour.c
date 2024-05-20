e65d18adfaf4af03fa1df5c3a18ae22594c108d8
SANSKAR SONTAKKE <76463065+SanskarSontakke@users.noreply.github.com>
2024-05-20 14:15:37 +0530

    Add files via upload

---
+++ Change_Colour.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void change_colour(int color_val,int bold)
+{
+    printf("\033[%d;3%dm",bold, color_val);
+}
+
+void change_colour_to_black()
+{
+    printf("\033[0;30m");
+}
+
+void change_colour_to_red()
+{
+    printf("\033[0;31m");
+}
+
+void change_colour_to_green()
+{
+    printf("\033[0;32m");
+}
+
+void change_colour_to_yellow()
+{
+    printf("\033[0;30m");
+}
+
+void change_colour_to_blue()
+{
+    printf("\033[0;30m");
+}
+
+void change_colour_to_purple()
+{
+    printf("\033[0;30m");
+}
+
+void change_colour_to_cyan()
+{
+    printf("\033[0;30m");
+}
+
+void change_colour_to_white()
+{
+    printf("\033[0;30m");
+}
+
